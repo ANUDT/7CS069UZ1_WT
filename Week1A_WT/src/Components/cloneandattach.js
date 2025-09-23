7fde9a7f3d9cdab556b6490bfc5f7ce2996d7c88
ANUDT <152813453+ANUDT@users.noreply.github.com>
2025-09-23 20:32:42 +0100

    tested and added shadow elements, created sections and boxes and css

--- Week1A_WT/src/Components/cloneandattach.js
+++ Week1A_WT/src/Components/cloneandattach.js
@@ -1,14 +1,17 @@
 
 //Clone and Attach element created to use template for mark up and styles
-var template = document.querySelector('my-template'); // get the template
-
 customElements.define('my-template', class extends HTMLElement { // define the class
   constructor() { // create a class that extends HTMLElement
     super(); // always call super() first in the ctor.
 
     // Create a shadow root and clone the template content into it
     let shadowRoot = this.attachShadow({ mode: 'open' }); // sets and returns 'this.shadowRoot'
-    shadowRoot.appendChild(template.content.cloneNode(true)); // true means cloning the content and elements
-    //then it will be added as a child of the shadow root
+    
+    // Get the template from the document
+    const template = document.querySelector('#my-template');
+    if (template) {
+      shadowRoot.appendChild(template.content.cloneNode(true)); // true means cloning the content and elements
+      //then it will be added as a child of the shadow root
     }
+  }
 });
